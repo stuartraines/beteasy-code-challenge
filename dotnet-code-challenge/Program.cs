dbfb535eaaafd153a7e9c60b749f21b266a7d2d2
stuartraines <stuart2424@yahoo.com>
2018-08-28 20:56:20 +0100

    Fleshing out program code to allow user to select file to process

--- dotnet-code-challenge/Program.cs
+++ dotnet-code-challenge/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using dotnet_code_challenge.Commands;
+using dotnet_code_challenge.Exceptions;
 using dotnet_code_challenge.Processors;
 using dotnet_code_challenge.Repositories;
 using dotnet_code_challenge.Requests;
@@ -21,21 +22,65 @@ namespace dotnet_code_challenge
 
         static void Main(string[] args)
         {
-            // 1. Ingest feed. 
-            /* This would otherwise be done out of process
-               but for purposes of this exercise, I ingest 
-               the feed first within this application. */
-            var input = "FeedData/Wolferhampton_Race1.json";
-            var result = _ingester.Ingest(input);
+            //TODO: Abstract to a Facade to allow for testability.
+            while (true)
+            {
+                DisplayInstructions();
 
-            Console.WriteLine($"result: {result}");
+                //TODO: Abstract away user input handling
+                var userResponse = Console.ReadLine().ToLowerInvariant();
+
+                if (userResponse == "q")
+                {
+                    break;
+                }
+
+                string file = string.Empty;
+
+                if (userResponse == "1")
+                {
+                    file = "FeedData/Caulfield_Race1.xml";
+                }
+                else if (userResponse == "2")
+                {
+                    file = "FeedData/Wolferhampton_Race1.json";
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option. Please try again");
+                    continue;
+                }
+
+                string feedId = null;
+
+                try
+                {
+                    // 1. Ingest feed. 
+                    /* This would otherwise be done out of process
+                       but for purposes of this exercise, I ingest 
+                       the feed first within this application. */
+                    feedId = _ingester.Ingest(file);
+                }
+                catch (FeedTypeNotSupportedException e)
+                {
+                    Console.WriteLine($"An exception occured while trying to process the input feed {userResponse}. Error details: {e.Message}.");
+                }
+
+                Console.WriteLine($"Feed ingested successfully with id: {feedId}");
+
+                // 2. Transform the feed data into a view model
 
 
-            // 2. Transform the feed data into a view model
+                // 3. Display the results
+            }
+        }
 
-
-
-            // 3. Display the results
+        private static void DisplayInstructions()
+        {
+            Console.WriteLine("Please select from one of the following options, or press 'q' to exit:");
+            Console.WriteLine();
+            Console.WriteLine("Press '1' for Caulfield_Race1.xml");
+            Console.WriteLine("Press '2' for Wolferhampton_Race1.json");
         }
     }
 }
