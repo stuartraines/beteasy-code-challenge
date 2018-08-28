61fa79fd7cbeda8a1cd4227f6f71bfb76e61aa90
stuartraines <stuart2424@yahoo.com>
2018-08-28 19:49:55 +0100

    Updating Test project to use NUnit + stubbing out program flow

--- dotnet-code-challenge.Test/UnitTest1.cs
+++ dotnet-code-challenge.Test/UnitTest1.cs
@@ -1,14 +1,13 @@
-using System;
-using Xunit;
+using NUnit.Framework;
 
 namespace dotnet_code_challenge.Test
 {
     public class UnitTest1
     {
-        [Fact]
+        [Test]
         public void Test1()
         {
-
+            Assert.That(true, Is.True);
         }
     }
 }
--- dotnet-code-challenge/Program.cs
+++ dotnet-code-challenge/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
-
-namespace dotnet_code_challenge
+﻿namespace dotnet_code_challenge
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1. Ingest feed. 
+            /* This would otherwise be done out of process
+               but for purposes of this exercise, I ingest 
+               the feed first within this application. */
+
+
+            // 2. Transform the feed data into a view model
+
+
+
+            // 3. Display the results
         }
     }
 }
